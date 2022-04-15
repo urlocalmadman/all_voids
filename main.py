f7934bdff22d393cfe281c320a9cf2c83012bfe6
urlocalmadman <tejas@vipin.app>
2022-04-15 15:49:21 +0530

    added all sprites

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import os
+
+variants = {0: 'Tartarean', 1: 'Intoxicating', 2: 'Unidentified', 3: 'Corrosive', 4: 'Elemental', 5: 'Fathomless', 6: 'Resolute',
+            7: 'Arcane', 8: 'Cryptic', 9: 'Illusive', 10: 'Ethereal', 11: 'Jaded', 12: 'Cataclysmic', 13: 'Celestial', 14: 'Ephemeral', 15: 'Phantom'}
+
+emotions = {'Abandonment': 5, 'Anger': 5, 'Anxious': 5, 'Cringe': 1, 'DownBad': 5, 'Envy': 5, 'Estrangement': 5, 'Gluttony': 5, 'Greed': 5, 'Jealous': 5, 'Judgement': 5,
+            'Lonely': 5, 'Lust': 5, 'Nostalgia': 5, 'Pain': 5, 'Paranoia': 5, 'Pride': 5, 'Sad': 5, 'Sadge': 4, 'Salty': 5, 'Sanctimony': 5, 'Sloth': 5, 'Sonder': 5, 'Spite': 5, 'Wrath': 5}
+
+
+for x in variants:
+    os.mkdir(fr'D:\desktop\all_vars\{variants[x]}')
+    for i in emotions:
+        os.mkdir(fr'D:\desktop\all_vars\{variants[x]}\{i}')
+        for j in range(1, emotions[i]+1):
+            test = requests.get(
+                f'https://img.voidpet.com/thumbnail/{i}{j}-{x}.jpg')
+
+            with open(fr'D:\desktop\all_vars\{variants[x]}\{i}\{j}.jpg', 'wb') as f:
+                f.write(test.content)
