eb3c2b64cf9dd7ebdd268fd4349c6796d6c5e63d
urlocalmadman <tejas@vipin.app>
2022-04-15 15:57:32 +0530

    changed python file to work more ubiquitously

--- main.py
+++ main.py
@@ -7,14 +7,15 @@ variants = {0: 'Tartarean', 1: 'Intoxicating', 2: 'Unidentified', 3: 'Corrosive'
 emotions = {'Abandonment': 5, 'Anger': 5, 'Anxious': 5, 'Cringe': 1, 'DownBad': 5, 'Envy': 5, 'Estrangement': 5, 'Gluttony': 5, 'Greed': 5, 'Jealous': 5, 'Judgement': 5,
             'Lonely': 5, 'Lust': 5, 'Nostalgia': 5, 'Pain': 5, 'Paranoia': 5, 'Pride': 5, 'Sad': 5, 'Sadge': 4, 'Salty': 5, 'Sanctimony': 5, 'Sloth': 5, 'Sonder': 5, 'Spite': 5, 'Wrath': 5}
 
+os.mkdir(r'.\all_variants')
 
 for x in variants:
-    os.mkdir(fr'D:\desktop\all_vars\{variants[x]}')
+    os.mkdir(fr'.\all_variants\{variants[x]}')
     for i in emotions:
-        os.mkdir(fr'D:\desktop\all_vars\{variants[x]}\{i}')
+        os.mkdir(fr'.\all_variants\{variants[x]}\{i}')
         for j in range(1, emotions[i]+1):
-            test = requests.get(
+            img = requests.get(
                 f'https://img.voidpet.com/thumbnail/{i}{j}-{x}.jpg')
 
-            with open(fr'D:\desktop\all_vars\{variants[x]}\{i}\{j}.jpg', 'wb') as f:
-                f.write(test.content)
+            with open(fr'.\all_variants\{variants[x]}\{i}\{j}.jpg', 'wb') as f:
+                f.write(img.content)
